c5d9e442276e12e2753600c7facf48ccdb56ce8b
thomas-g-knowles <thomas.g.knowles@gmail.com>
2022-03-23 15:37:20 +0000

    commit thing

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,13 @@
 const Footer = () => {
   return (
     <div className="Footer">
-      <p>This is a footer</p>
+      <h2>Project made by:</h2>
+      <ul>
+        <li>Cherrelle Ward</li>
+        <li>Christopher Benson</li>
+        <li>Josh West</li>
+        <li>Thomas Knowles</li>
+      </ul>
     </div>
-  );
-};
-
-export default Footer;
+  )
+}
