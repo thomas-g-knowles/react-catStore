8581ff4ff647eb79ab99ed0330f1eb12aac73157
Chris Benson <ChrisBenson87@gmail.com>
2022-03-25 12:31:54 +0000

    added Chris

--- src/component/Basket.jsx
+++ src/component/Basket.jsx
@@ -5,17 +5,18 @@ const Basket = (props) => {
     if(!props.show){return null}
 
     return (
-<div className = "basketModel">
-    {props.basket.map((item) => (
-    <div>
-      <h3>{item.name}</h3>
-      <h3>{item.cat}</h3>
-      <h3>{item.country}</h3>
-      <h3>{item.price}</h3>
-    </div>
-    ))}
-</div>
-  );
-};
+      <div className = "basketModel">
+          {props.basket.map((item, index) => (
+          <div>
+            <img key={index} src={item.url} alt="image of cat" />
+            <h3>{item.name}</h3>
+            <h3>£ {item.price}</h3>
+            <button onClick={() => props.removeItem(index)}>Remove Cat</button>
+          </div>
+          ))}
+          <h4>Total Price: £ {props.sum}</h4>
+      </div>
+        );
+      };
 
 export default Basket;
